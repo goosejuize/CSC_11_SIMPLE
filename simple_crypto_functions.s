c55d294d11df17965f9287f17a513467de3a1580
goosejuize <135565093+goosejuize@users.noreply.github.com>
2023-12-10 19:05:29 -0800

    simple_crypto_functions.s

--- simple_crypto_functions.s
+++ simple_crypto_functions.s
@@ -1,4 +1,4 @@
-global encrypt
+.global encrypt
 .global decrypt
 
 .text
@@ -19,7 +19,7 @@ EN_WHL_R5_LT_R1:
         CMP R5, R1                      // step #4_en - compare R5 and R1 in this order of the registers
         BGE EN_END_WHL_R5_LT_R1         // step #5_en - if R5 greater than or equal to R1, branch to EN_END_WHL_R5_LT_R1
         LDRB R6, [R0, R5]               // step #6_en - load byte into register R6, the contents of R0[R5]
-        LDRB R7, [R2, R4]                       // step #7_en - load byte into register R7, the contents of R2[R4]
+        LDRB R7, [R2, R4]               // step #7_en - load byte into register R7, the contents of R2[R4]
         EOR R6, R6, R7                  // step #8_en - exclusive or R6 and R7, storing result in R6
         AND R7, R5, #255                // step #9_en - use bitwise AND with R5 and #255 to cast R5's value as char instead of int, store $
         ADD R6, R6, R7                  // step #10_en - add R6 and R7, storing result in R6
@@ -46,3 +46,27 @@ EN_END_WHL_R5_LT_R1:
 ///
 .align 4
 decrypt:
+        PUSH { R4, R5, R6, R7, LR }     // step #1_de - Save R4,R5,R6,R7, and LR
+        MOV R4, #0                      // step #2_de - Set register R4 to 0
+        MOV R5, #0                      // step #3_de - Set register R5 to 0
+DE_WHL_R5_LT_R1:
+        CMP R5, R1                      // step #4_de - compare R5 and R1 in this order of the registers
+        BGE DE_END_WHL_R5_LT_R1         // step #5_de - if R5 greater than or equal to R1, branch to DE_END_WHL_R5_LT_R1
+        LDRB R6, [R0, R5]               // step #6_de - load byte into register R6, the contents of R0[R5]
+        AND R7, R5, #255                // step #7_de - use bitwise AND with R5 and #255 to cast R5's value as char instead of int, store $
+        SUB R6, R6, R7                  // step #8_de - subtract R6 and R7, storing result in R6
+        LDRB R7, [R2, R4]               // step #9_de - load byte into register R7, the contents of R2[R4]
+        EOR R6, R6, R7                  // step #10_de - exclusive or R6 and R7, storing result in R6
+        STRB R6, [R0, R5]                       // step #11_de - store byte in R6 to R0[R5]
+        ADD R4, R4, #1                  // step #12_de - increment R4 by one
+DE_IF_R4_GE_R3:
+        CMP R4, R3                      // step #13_de - compare R4 and R3 in this order of the registers
+        MOVGE R4, #0                    // step #14_de - do conditional execution, move if greater than or equal, into R4 the value #0
+        ADD R5, R5, #1                  // step #15_de - increment R5 by one
+        B DE_WHL_R5_LT_R1               // step #16_de - branch (or branch ALways) back to DE_WHL_R5_LT_R1 label
+DE_END_WHL_R5_LT_R1:
+        POP { R4, R5, R6, R7, PC }      // step #17_en - restore the registers R4,R5,R6,R7, and PC; in the order of registers given
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+/// end of decrypt function //////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
